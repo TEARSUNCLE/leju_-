3b0fda97fbd39cd7254809eedf6b7e6be6875432
刘孝林 <13106917538@163.com>
2022-07-04 15:51:06 +0800

    02-删除多余的文件，配置开发环境

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,5 +15,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'prettier/prettier': 'off',
+    // 关闭组件命令规则
+    "vue/multi-word-component-names": "off",
   },
 };
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+
+  publicPath: '/',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: process.env.NODE_ENV === 'development',
+  productionSourceMap: false,
+
+  devServer: {
+    open:true,
+
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        pathRewrite: {
+          '/api': ''
+        },
+      }
+    }
+  },
 });
